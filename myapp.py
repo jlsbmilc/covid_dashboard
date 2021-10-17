6443a492b8f62737530784a0d57cec0eeceff621
jlsbmilc <62806745+jlsbmilc@users.noreply.github.com>
2021-10-17 20:52:57 +0300

    commit

--- myapp.py
+++ myapp.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 df_raw = pd.read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
 
@@ -11,7 +13,24 @@ selection = st.selectbox('Please select a location:', locations)
 data = df_raw[df_raw['location'] == selection]
 fig = px.line(data, x='date', y='total_cases')
 fig2 = px.bar(data, x='date', y='new_cases')
-# fig.add_trace(fig2)
 st.plotly_chart(fig)
 
 st.plotly_chart(fig2)
+
+fig3 = make_subplots(specs=[[{"secondary_y": True}]])
+
+
+fig3.add_trace(
+    go.Bar(x=data['date'], y=data['new_cases'], name="Daily new cases in time"),
+    secondary_y=False
+)
+
+fig3.add_trace(
+    go.Scatter(x=data['date'], y=data['stringency_index'], name="Stringency index in time", mode='lines'),
+    secondary_y=True
+)
+
+
+st.write("Chart 3")
+
+st.plotly_chart(fig3)
